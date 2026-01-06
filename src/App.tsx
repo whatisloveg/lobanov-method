9543d856bb3279d8237f77599029ec63b8d4707a
Vladislav <95077912+whatisloveg@users.noreply.github.com>
2026-01-07 01:23:31 +0300

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/lobanov-method">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
